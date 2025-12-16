e523924cbef9eddb26e4a308476feb72ce4b4654
Leonenko <kerkleonsk@gmail.com>
2025-12-16 18:39:41 +0300

    Signed-off-by: Leonenko <kerkleonsk@gmail.com>

---
+++ 2 — копия.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <cmath>
+Just deleted some symbols
+int main() {
+    double INTERVAL_START = 0.0;
+    double INTERVAL_END = 6.0;
+    added const
+    double s = 12.0
+    Editing symbols 0_0
