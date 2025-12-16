d64f8a93055897fc897df02bb4a39bde219bd5b2
Leonenko <kerkleonsk@gmail.com>
2025-12-16 18:55:36 +0300

    my files

---
+++ Lab4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+int main() {
+    setlocale(LC_ALL,"Russian");
+    srand(static_cast<unsigned int>(time(0)));
---
+++ Laborotory work №1 — копия.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <cmath>
+Adding comments
+int main() {
+	int choice;
+	int usernumber;
+	setlocale(LC_ALL, "Ru");
